5f71170be179be2ae391a54fb874b2811affecc0
PreVendasNDD <joao.pinheiro@ndd.tech>
2026-01-28 16:33:15 -0300

    Create AppStyles.js

---
+++ src/styles/AppStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  background: #f5f5f5;
+  overflow: hidden;
+`;
+
+export const MainContent = styled.div`
+  flex: 1;
+  padding: 20px;
+  overflow: auto;
+  background: white;
+`;
